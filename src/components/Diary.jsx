284d690c1c1cf9836045b2819044bf8c82141721
Hongseongjun-dev <opus706@naver.com>
2023-04-26 20:44:17 +0900

    seongjun

--- src/components/Diary.jsx
+++ src/components/Diary.jsx
@@ -4,16 +4,38 @@ const Diary = () => {
   // 이벤트 핸들러부터 state까지 모두 스스로 작성해보시길 바랍니다!
   // 과연 어떤 state를 생성해주어야하고, 또 어떤 이벤트 핸들러가 필요할까요?
   // 실습으로 다뤘던 두 문제를 참고하면 충분히 해결할 수 있으실거라 생각합니다!
+  const handleClick1= () =>{
+    alert(`제목= ${subject} \n내용= ${content}`)//제목과 내용을 출력
+  };
+
+  const handleClick2= () =>{
+    setSubject("")
+    setContent("")
+    //placeholder = "제목을 입력해주세요"//초기화
+
+  };
+
+  const[subject, setSubject] = useState("");
+  const[content, setContent] = useState("");
+  
+  const handleSubject = (e) => { //입력한 제목과 내용을 alert로 출력
+    setSubject(e.target.value);
+  };
+
+  const handleContent = (t) => { //입력한 제목과 내용을 alert로 출력
+    setContent(t.target.value);
+  };
 
   return (
     <div className="diary">
-      <input name="subject" placeholder="제목을 입력해주세요" />
+      <input name="subject" placeholder="제목을 입력해주세요" onChange={handleSubject} value={subject}/>
       <br />
-      <textarea name="content" placeholder="내용을 입력해주세요" />
+      <textarea name="content" placeholder="내용을 입력해주세요" onChange={handleContent} value={content}/>
       <br />
-      <button>초기화</button>
+      <button onClick={handleClick2}>초기화</button>
       <br />
-      <button>입력 완료</button>
+      <button onClick={handleClick1}>입력 완료</button>
+    
     </div>
   );
 };
